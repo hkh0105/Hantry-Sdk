52e51eac65111dc501db6e14ca4ac78e56892766
hkh9601 <hkh9601@gmail.com>
2022-07-21 23:21:19 +0900

    Fix : add Umd

--- packages/react/webpack.config.js
+++ packages/react/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "index.js",
-    type: "umd",
+    libraryTarget: "umd",
     umdNamedDefine: true,
   },
 };
