e674d14af0f516b257c54e3db82492112b5ced15
hkh9601 <hkh9601@gmail.com>
2022-07-21 21:49:25 +0900

    FEA: Webpack Bundling

---
+++ packages/node/.babelrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: ["@babel/preset-env"],
+  env: {
+    build: {
+      ignore: ["__tests__"],
+    },
+  },
+};
---
+++ packages/profiler/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js"],
+  },
+  entry: "./lib/profiler.js",
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        options: {
+          configFile: path.resolve(__dirname, ".babelrc.js"),
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+  },
+};
